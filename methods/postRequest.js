557d761a7affa075d32c6d0875a3bb01c80c1daa
Shreya Sarker <shreya.sarker@gmail.com>
2023-04-04 19:55:51 +0600

    Added API for Posting data

--- methods/postRequest.js
+++ methods/postRequest.js
@@ -1,1 +1,24 @@
-export default (req, res) => {};
+import booksBodyParser from "../utils/books.body-parser.js";
+import books from "../data/books.json" assert { type: "json"};
+
+export default async (req, res) => {
+  if(req.url === "/api/books") {
+    try {
+      let body = await booksBodyParser(req);
+      // console.log(body);
+      if(books !== undefined && books.length > 0) {
+        let id = parseInt(books[books.length - 1]._id) + 1;
+        body._id = id;
+        console.log(id);
+      } else {
+        body._id = 1;
+      }
+      req.books.push(body);
+      res.writeHead(201, {"Content-Type": "application/json"});
+      res.end();
+    } catch(err) {
+      res.writeHead(400, {"Content-Type": "application/json"});
+      res.end(JSON.stringify({title: "Error", message: "Something went wrong!"}));
+    }
+  }
+};
